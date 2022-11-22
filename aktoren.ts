cf7ecb56d3dfb3af173bef1d01741ac303414255
SamuelBaw <104888073+SamuelBaw@users.noreply.github.com>
2022-11-22 14:09:37 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, aktoren.ts, myOLED.ts

--- aktoren.ts
+++ aktoren.ts
@@ -4,13 +4,16 @@
 //% color="#FF33B2" icon="\uf085"
 namespace smartfeldAktoren {
 
+
+    let strip = new neopixel.Strip();
+
     /**
     * Spins the motor in one direction at full speed
     * @param pin Which pin the motor is on
     */
-    //% subcategory="Servo" weight=100
+    //% subcategory="Servo" weight=100 color=#2699BF
     //% group="360° Servo 114992423"
-    //% blockId=spin_one_way weight=100
+    //% blockId=spin_one_way 
     //% block="spin one way pin %pin"
     export function spin_one_way(pin = AnalogPin.P1): void {
         pins.servoWritePin(pin, 180)
@@ -20,9 +23,9 @@ namespace smartfeldAktoren {
     * Spins the motor in other direction at full speed
     * @param pin Which pin the motor is on
     */
-    //% subcategory="Servo" weight=100
+    //% subcategory="Servo" weight=80 color=#2699BF
     //% group="360° Servo 114992423"
-    //% blockId=spin_other_way weight=80
+    //% blockId=spin_other_way 
     //% block="spin other way pin %pin"
     export function spin_other_way(pin = AnalogPin.P2): void {
         pins.servoWritePin(pin, 0)
@@ -33,9 +36,9 @@ namespace smartfeldAktoren {
     * @param pin Which pin the motor is on
     * @param speed Speed from 0 to 100
     */
-    //% subcategory="Servo" weight=100
+    //% subcategory="Servo" weight=60 color=#2699BF
     //% group="360° Servo 114992423"
-    //% blockId=spin_one_way_with_speed weight=60
+    //% blockId=spin_one_way_with_speed
     //% block="spin one way pin %pin | with speed %speed"
     //% speed.min=0 speed.max=100
     export function spin_one_way_with_speed(pin = AnalogPin.P1, speed = 50): void {
@@ -48,9 +51,9 @@ namespace smartfeldAktoren {
     * @param pin Which pin the motor is on
     * @param speed Speed from 0 to 100
     */
-    //% subcategory="Servo" weight=100
+    //% subcategory="Servo" weight=40 color=#2699BF
     //% group="360° Servo 114992423"
-    //% blockId=spin_other_way_with_speed weight=40
+    //% blockId=spin_other_way_with_speed
     //% block="spin other way pin %pin | with speed %speed"
     //% speed.min=0 speed.max=100
     export function spin_other_way_with_speed(pin = AnalogPin.P2, speed = 50): void {
@@ -62,25 +65,126 @@ namespace smartfeldAktoren {
     * Turns off the motor at this pin
     * @param pin Which pin the motor is on
     */
-    //% subcategory="Servo" weight=100
+    //% subcategory="Servo" weight=20 color=#2699BF
     //% group="360° Servo 114992423"
-    //% blockId=turn_off_motor weight=20
+    //% blockId=turn_off_motor 
     //% block="turn off motor at pin %pin"
     export function turn_off_motor(pin = DigitalPin.P1): void {
         pins.digitalWritePin(pin, 0)
     }
 
-    /**
-    * Turns off the motor at this pin
-    * @param pin Which pin the motor is on
-    */
-    //% subcategory="Servo" weight=100
-    //% group="360° Servo 114992425"
-    //% blockId=turn_off_motor weight=20
-    //% block="turn off motor at pin %pin"
-    export function turn_off_moto1r(pin = DigitalPin.P1): void {
-        pins.digitalWritePin(pin, 0)
+
+//--------------------------------------------------------------------------------------------------------
+
+
+    //% subcategory="LED" weight=100 color=#2699BF
+    //% group="RGB Strip 104020131"
+    //% block="erstelle Strip " block="NeoPixel at pin %pin|with %numleds|leds as %mode"
+    //% parts="neopixel"
+    //% trackArgs=0,2
+    //% blockSetVariable=strip
+    export function create(pin: DigitalPin, numleds: number, mode: NeoPixelMode): neopixel.Strip {
+        return neopixel.create(pin, numleds, mode);
     }
 
+    /**
+ * Create a range of LEDs.
+ * @param start offset in the LED strip to start the range
+ * @param length number of LEDs in the range. eg: 4
+ */
+    //% subcategory="LED" weight=99
+    //% group="RGB Strip 104020131"
+    //% blockId="neopixel_range" block="%strip|range from %start|with %length|leds"
+    //% strip.defl=neopixel_set_strip_rainbow
+    //% blockSetVariable=range
+    export function range(start: number, length: number): neopixel.Strip {
+        return strip.range(start, length);
+    }
+
+    /**
+     * Shows all LEDs to a given color (range 0-255 for r, g, b).
+     * @param rgb RGB color of the LED
+     */
+    //% subcategory="LED" weight=95
+    //% group="RGB Strip 104020131"
+    //% blockId="neopixel_set_strip_color" block="%strip|show color %rgb=neopixel_colors"
+    //% strip.defl=strip
+    export function showColor(rgb: number) {
+        return strip.showColor(rgb);
+    }
+
+
+    //% subcategory="LED" weight=90 color=#2699BF
+    //% group="RGB Strip 104020131" 
+    //% block="setze RGB Wert " block="red %red|green %green|blue %blue" 
+    export function rgb(red: number, green: number, blue: number): number {
+        return neopixel.rgb(red, green, blue);
+    }
+
+    /**
+     * Shows a rainbow pattern on all LEDs.
+     * @param startHue the start hue value for the rainbow, eg: 1
+     * @param endHue the end hue value for the rainbow, eg: 360
+     */
+    //% subcategory="LED" weight=85
+    //% group="RGB Strip 104020131"
+    //% blockId="neopixel_set_strip_rainbow" block="%strip|show rainbow from %startHue|to %endHue"
+    //% strip.defl=strip
+    export function showRainbow(startHue: number = 1, endHue: number = 360) {
+        return strip.showRainbow(startHue, endHue);
+    }
+
+    /**
+     * Displays a vertical bar graph based on the `value` and `high` value.
+     * If `high` is 0, the chart gets adjusted automatically.
+     * @param value current value to plot
+     * @param high maximum value, eg: 255
+     */
+    //% subcategory="LED" weight=84
+    //% group="RGB Strip 104020131"
+    //% blockId=neopixel_show_bar_graph block="%strip|show bar graph of %value|up to %high"
+    //% strip.defl=strip
+    //% icon="\uf080"
+    export function showBarGraph(value: number, high: number): void {
+        return strip.showBarGraph(value, high);
+    }
+
+
+    /**
+     * Send all the changes to the strip.
+     */
+    //% subcategory="LED" weight=79
+    //% group="RGB Strip 104020131"
+    //% blockId="neopixel_show" block="%strip|show" blockGap=8
+    //% strip.defl=strip
+    export function show() {
+        return strip.show();
+    }
+
+    /**
+     * Turn off all LEDs.
+     * You need to call ``show`` to make the changes visible.
+     */
+    //% subcategory="LED" weight=76
+    //% group="RGB Strip 104020131"
+    //% blockId="neopixel_clear" block="%strip|clear"
+    //% strip.defl=strip
+    export function clear(): void {
+        strip.clear();
+        strip.show();
+    }
+
+    /**
+     * Shift LEDs forward and clear with zeros.
+     * You need to call ``show`` to make the changes visible.
+     * @param offset number of pixels to shift forward, eg: 1
+    */
+    //% subcategory="LED" weight=40
+    //% group="RGB Strip 104020131"
+    //% blockId="neopixel_shift" block="%strip|shift pixels by %offset" blockGap=8
+    //% strip.defl=strip
+    export function shift(): void {
+        strip.shift();
+    }
 
 }
--- main.ts
+++ main.ts
@@ -1,6 +1,26 @@
-OLED.drawLine(
-0,
-0,
-20,
-20
-)
+input.onButtonPressed(Button.A, function () {
+    laeuft = 0
+    OLED.clear()
+})
+let laeuft = 0
+OLED.init(128, 64)
+laeuft = 1
+let strip = neopixel.create(DigitalPin.P2, 10, NeoPixelMode.RGB)
+let range = strip.range(0, 8)
+basic.forever(function () {
+    if (laeuft) {
+        basic.pause(1000)
+        OLED.writeString("UV: ")
+        OLED.writeNumNewLine(smartfeldSensoren.hw837_getUV(AnalogPin.P0))
+    }
+})
+basic.forever(function () {
+    if (laeuft) {
+        strip.showColor(smartfeldAktoren.rgb(54, 255, 220))
+        basic.pause(1000)
+        range.showRainbow(1, 360)
+        basic.pause(1000)
+        strip.clear()
+        basic.pause(5000)
+    }
+})
