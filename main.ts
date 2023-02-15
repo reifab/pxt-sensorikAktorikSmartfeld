158f55ed563b2d66dd62165d2729deba14477c19
SamuelBaw <104888073+SamuelBaw@users.noreply.github.com>
2023-02-15 13:49:26 +0100

    Change to template main

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
-smartfeldSensoren.initGas()
 basic.forever(function () {
-    serial.writeValue("CO2", smartfeldSensoren.measReadCO2eq())
-    serial.writeValue("tVOC", smartfeldSensoren.measReadtVOC())
-    if (smartfeldSensoren.measReadCO2eq() < 50) {
-        basic.showIcon(IconNames.Happy)
-    } else {
-        basic.showIcon(IconNames.Happy)
-    }
-    basic.pause(1000)
+    basic.showIcon(IconNames.Target)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(500)
 })
