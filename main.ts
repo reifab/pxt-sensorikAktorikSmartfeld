b341bb27e43d3732d2c245545ea7bdd14eb4a7d0
SamuelBaw <104888073+SamuelBaw@users.noreply.github.com>
2023-03-01 09:27:44 +0100

    REM: Main-Loop

--- main.ts
+++ main.ts
@@ -1,6 +1,1 @@
-basic.forever(function () {
-    basic.showIcon(IconNames.Target)
-    basic.pause(500)
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(500)
-})
+
--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+basic.forever(function () {
+    basic.showIcon(IconNames.Target)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.pause(500)
+})
